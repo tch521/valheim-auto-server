2a0adbe6f619e2fb63665596b4f87dcbfc0cb5e2
Todd <73101886+tch521@users.noreply.github.com>
2025-11-22 20:59:16 +1030

    mostly working

---
+++ InstanceController/app.py
@@ -0,0 +1,198 @@
+import os
+import boto3
+import logging
+from botocore.exceptions import ClientError
+import json
+from datetime import datetime, timedelta, UTC
+from tempfile import TemporaryDirectory
+import ipaddress
+
+# Setup logging
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s %(levelname)s %(message)s"
+)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+EC2 = boto3.client("ec2")
+
+
+def lambda_handler(event, context):
+    try:
+        response = main(event, context)
+        return {"statusCode": 200, "body": json.dumps(response)}
+    except AssertionError as e:
+        return {"statusCode": 400, "body": str(e)}
+    except PermissionError as e:
+        return {"statusCode": 403, "body": str(e)}
+
+
+def main(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # if event comes from api gateway, log details then extract body
+    if "body" in event:
+        LOGGER.info(f"API Gateway event received: {json.dumps(event, indent=2)}")
+        event = json.loads(event["body"])
+        assert (
+            "RequestType" in event
+        ), "Invalid API request body. 'RequestType' is required."
+        RequestType = event["RequestType"]
+        checkApiSourceIp(event, log_message_prefix=f"{RequestType} request from ")
+    else:
+        # event comes direct (i.e. from eventbridge scheduler)
+        RequestType = event["RequestType"]
+
+    actions = {
+        "start_valheim_server": lambda: [
+            createBackup(),
+            startInstance(os.environ["ValheimEC2InstanceId"]),
+        ][1],
+        "stop_valheim_server": lambda: stopInstance(
+            os.environ["ValheimEC2InstanceId"], force=False
+        ),
+        "force_stop_valheim_server": lambda: stopInstance(
+            os.environ["ValheimEC2InstanceId"], force=True
+        ),
+    }
+
+    # Now execute request
+    if RequestType in actions:
+        return actions[RequestType]()
+    else:
+        raise AssertionError(
+            f"Invalid RequestType: {RequestType}. "
+            f"RequestType must be one of: {list(actions.keys())}"
+        )
+
+
+def checkApiSourceIp(event, log_message_prefix=None):
+    """Checks if the source IP of the API request is in the allowed list."""
+    prefixListId = os.environ.get("AllowedIpPrefixListId")
+    response = EC2.get_managed_prefix_list_entries(PrefixListId=prefixListId)
+    source_ip = ipaddress.ip_address(event["requestContext"]["identity"]["sourceIp"])
+    for entry in response["Entries"]:
+        if source_ip in ipaddress.ip_network(entry["Cidr"]):
+            LOGGER.info(f"{log_message_prefix}{entry['Description']}")
+            return
+    raise PermissionError(
+        f"{log_message_prefix}Source IP {source_ip} not in allowed list."
+    )
+
+
+def get_instance_state(InstanceId):
+    try:
+        response = EC2.describe_instances(InstanceIds=[InstanceId])
+        state = response["Reservations"][0]["Instances"][0]["State"]["Name"]
+        return state
+    except ClientError as e:
+        LOGGER.error(f"Error getting instance state: {e.response['Error']['Message']}")
+        raise
+
+
+def startInstance(InstanceId):
+    LOGGER.info(f"Checking instance status: {InstanceId}")
+    instance_state = get_instance_state(InstanceId)
+    if instance_state != "running":
+        LOGGER.info(f"Starting EC2 instance: {InstanceId}")
+        EC2.start_instances(InstanceIds=[InstanceId])
+        LOGGER.info(f"Started EC2 instance: {InstanceId}")
+        return {"Message": f"EC2 instance {InstanceId} is starting."}
+    else:
+        LOGGER.info(f"Instance already running: {InstanceId}")
+        return {"Message": f"EC2 instance {InstanceId} is already running."}
+
+
+def createBackup():
+    new_name = f"valheim_config_backup_{datetime.now():%Y-%m-%d_%H-%M-%S}.tar.gz"
+    LOGGER.info(f"Creating backup on S3 {new_name}")
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(os.environ["ValheimS3BucketName"])
+    with TemporaryDirectory() as tmpdir:
+        for obj in bucket.objects.filter(Prefix="config/"):
+            target = os.path.join(tmpdir, os.path.relpath(obj.key, "config/"))
+            os.makedirs(os.path.dirname(target), exist_ok=True)
+            bucket.download_file(obj.key, target)
+        archive_path = os.path.join(tmpdir, new_name)
+        os.system(f"tar -czf {archive_path} -C {tmpdir} config")
+        bucket.upload_file(archive_path, new_name)
+    # also delete old backups
+    backups = sorted(
+        [
+            obj
+            for obj in bucket.objects.all()
+            if obj.key.startswith("valheim_config_backup_")
+        ],
+        key=lambda x: x.key,
+        reverse=True,
+    )
+    if len(backups) < 5:
+        for old_backup in backups[5:]:
+            LOGGER.info(f"Deleting old backup {old_backup.key}")
+            old_backup.delete()
+
+
+def instance_in_use(InstanceId, lookback_hours=1, usage_threshold=10.0, period=300):
+    cw = boto3.client("cloudwatch")
+    try:
+        end_time = datetime.now(UTC)
+        start_time = end_time - timedelta(hours=lookback_hours)
+
+        response = cw.get_metric_statistics(
+            Namespace="AWS/EC2",
+            MetricName="CPUUtilization",
+            Dimensions=[{"Name": "InstanceId", "Value": InstanceId}],
+            StartTime=start_time,
+            EndTime=end_time,
+            Period=period,
+            Statistics=["Average"],
+        )
+
+        expected_datapoints = lookback_hours * 3600 // period
+        actual_datapoints = len(response["Datapoints"])
+        if (
+            actual_datapoints < expected_datapoints * 0.9
+        ):  # a few of the latest datapoints may not be available
+            LOGGER.info(f"Instance {InstanceId} was recently turned on - not stopping.")
+            return True
+
+        for datapoint in response["Datapoints"]:
+            if datapoint["Average"] > usage_threshold:
+                LOGGER.info(
+                    f"Instance {InstanceId} has recent CPU activity - not stopping."
+                )
+                return True
+        # If all checks pass, then instance is not in use
+        return False
+
+    except ClientError as e:
+        LOGGER.error(
+            f"Error checking CPU metrics: {e.response['Error']['Message']}",
+            exc_info=True,
+        )
+        return True
+
+
+def stopInstance(InstanceId, force=False):
+    LOGGER.info(f"Checking instance status: {InstanceId}")
+    instance_state = get_instance_state(InstanceId)
+    if instance_state == "running":
+        if force:
+            LOGGER.info(f"Stopping EC2 instance: {InstanceId}")
+            EC2.stop_instances(InstanceIds=[InstanceId])
+            LOGGER.info(f"Stopped EC2 instance: {InstanceId}")
+            return {"Message": f"EC2 instance {InstanceId} is stopping."}
+        else:
+            # Check CPU usage before stopping
+            if instance_in_use(InstanceId):
+                return {
+                    "Message": f"EC2 instance {InstanceId} has recent CPU activity - not stopping."
+                }
+            else:
+                LOGGER.info(f"Stopping inactive EC2 instance: {InstanceId}")
+                EC2.stop_instances(InstanceIds=[InstanceId])
+                LOGGER.info(f"Stopped EC2 instance: {InstanceId}")
+                return {"Message": f"Inactive EC2 instance {InstanceId} is stopping."}
+    else:
+        LOGGER.info(f"Instance already stopped: {InstanceId}")
+        return {"Message": f"EC2 instance {InstanceId} is not running."}
---
+++ utils/filter_instances.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+df = pd.read_csv(Path(__file__).parent / "instanceTypes.csv")
+
+filtered_df = df[
+    (df["vCPUs"] >= 2)
+    & (df["vCPUs"] <= 4)
+    & (df["Memory (GiB)"] >= 8)
+    & (df["Memory (GiB)"] <= 16)
+    & (df["Architecture"].str.contains("x86_64"))
+    & (df["Current generation"] == True)
+]
+
+filtered_df = filtered_df.sort_values(by="On-Demand Linux pricing")
+print(filtered_df.head(20).iloc[:, [0, 8, 14, 15, 22, 45]])
+
+filtered_df.to_csv(Path(__file__).parent / "filtered_instances.csv", index=False)
